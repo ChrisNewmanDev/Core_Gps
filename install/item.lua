39a5695f2b20d6ae394b3ef982b9e595864fafdd
Chris Newman <jjpienaar@hotmail.co.za>
2026-01-19 15:52:14 +0200

    Changes - Still in testing phase.

---
+++ install/item.lua
@@ -0,0 +1,12 @@
+core_gps = {
+    name = 'core_gps',
+    label = 'GPS',
+    weight = 200,
+    type = 'item',
+    image = 'core_gps.png',
+    unique = true,
+    useable = true,
+    shouldClose = true,
+    combinable = nil,
+    description = 'A GPS device for marking and managing locations'
+}
